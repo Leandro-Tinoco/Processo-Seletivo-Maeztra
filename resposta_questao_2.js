2a0ee382dcc824e61db01cab80c627929d2805db
Leandro-Tinoco <leandro.tinoco.pinheiro@gmail.com>
2021-03-22 21:48:30 -0300

    Respostas Teste Lógico

--- resposta_questao_2.js
+++
@@ -1,7 +0,0 @@
-var numeros = [4, 5, 44, 98, 4, 5, 6, 7];
-
-function verificaRepetidos(num) {
-    var novoArray = num.filter((res, i) => num.indexOf(res) !== i);
-    console.log("Resultado dos números repetidos -> " + novoArray)
-}
-verificaRepetidos(numeros);
--- resposta_questao_5.js
+++
@@ -1,8 +0,0 @@
-function retornaPosicoes(num) {
-    let total = num
-    for (let i = 2; i < num; i++) {
-        total = i * total;
-    }
-    console.log(total)
-}
-retornaPosicoes(3);
